ff1238fbe89e1449e6109935908ddc5a249f1c12
TarcisioBalbi <77362801+TarcisioBalbi@users.noreply.github.com>
2021-05-30 20:00:04 -0300

    Update hand_media_controls.py

--- hand_gesture_media_control/hand_media_controls.py
+++ hand_gesture_media_control/hand_media_controls.py
@@ -18,58 +18,70 @@ keyboard = Controller()
 cap = cv2.VideoCapture(0)
 handPoseIdentifier = hpid.handPoseId()
 
-#For a command to be valid, this flag must be on
 commandFlag = 0
-
+# hangloose = command enabeler
+# 1 = Play/Pause
+# 2 = Volume up
+# 3 = Volume down
+# 4 = Next track
 while True:
     propList = []
     sucess,img = cap.read()
-    #Checks the currente pose and controls the PC media depending on the pose.
-    img,pose = handPoseIdentifier.getHandPose(img)
-    print(pose)
     
-    #The 'hangloose' gesture triggers the command flag
-    # After the 'hangloose', a command can be given
+    img,pose = handPoseIdentifier.getHandPose(img)
+    
+    
     if pose =='hangloose':
-        commandFlag =1
+        if not commandFlag:
+            print('Ready for command.')
+            commandFlag =1
         
     if pose == '1' and commandFlag==1:
+        print('Play/Pause')
         keyboard.press(Key.media_play_pause )
         keyboard.release(Key.media_play_pause )
         commandFlag = 0
         
+        
     elif pose == '2' and commandFlag==1:
-        keyboard.press(Key.ctrl_l)
-        keyboard.press(Key.media_volume_up )
-        keyboard.release(Key.media_volume_up )
-        keyboard.press(Key.media_volume_up )
-        keyboard.release(Key.media_volume_up )
-        keyboard.press(Key.media_volume_up )        
-        keyboard.release(Key.media_volume_up )
-        keyboard.release(Key.ctrl_l)
-        commandFlag = 0
-    elif pose == '3' and commandFlag==1:
-        keyboard.press(Key.ctrl_l)
-        keyboard.press(Key.media_volume_down )
-        keyboard.release(Key.media_volume_down )
-        keyboard.press(Key.media_volume_down )
-        keyboard.release(Key.media_volume_down )
-        keyboard.press(Key.media_volume_down )        
-        keyboard.release(Key.media_volume_down )
-        keyboard.release(Key.ctrl_l)
-        commandFlag = 0
-    elif pose == '4' and commandFlag==1:
-        keyboard.press(Key.ctrl_l)
-        keyboard.press(Key.media_next )
-        keyboard.release(Key.media_next )
-        keyboard.release(Key.ctrl_l)
+        print('Volume up')
+        while pose=='2':
+            keyboard.press(Key.media_volume_up )
+            keyboard.release(Key.media_volume_up )
+            _,img = cap.read()
+            img,pose = handPoseIdentifier.getHandPose(img)
+            #cv2.imshow('Image', img)
+            #cv2.waitKey(1)
+            time.sleep(0.05)
+            
         commandFlag = 0
         
-    cv2.imshow('Image', img)
-    
+    elif pose == '3' and commandFlag==1:
+        print('Volume down')
+        while pose=='3':
+            keyboard.press(Key.media_volume_down )
+            keyboard.release(Key.media_volume_down )
+            _,img = cap.read()
+            img,pose = handPoseIdentifier.getHandPose(img)
+            #cv2.imshow('Image', img)
+            #cv2.waitKey(1)
+            time.sleep(0.2)
+        
+        commandFlag = 0
+        
+    elif pose == '4' and commandFlag==1:
+        print('Next track')
+        keyboard.press(Key.media_next )
+        keyboard.release(Key.media_next )
+        commandFlag = 0
+        
+        
+    #cv2.imshow('Image', img)
+    cv2.imshow('Esc or Enter to close', np.zeros((200,200)))
     key = cv2.waitKey(1)
     if key == 27 or key == 10: ## ESC or ENTER to break
         break
 
-cv2.destroyWindow('Image')
+cv2.destroyAllWindows()
 cap.release() 
+
